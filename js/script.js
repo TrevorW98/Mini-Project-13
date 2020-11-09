54a35239ecfb3095b2e5ad69c0bbe5f48732dbea
Trevor <Twomack6823@gmail.com>
2020-11-08 18:33:27 -0800

    everything but the font now

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Trevor Womack 
+// 11/08/2020
+// Mini Challenge 13
+// This challenge has an upvote button and comments, showcased at the Web Unleashed Conference 2020
+
 let upvote = document.getElementById("upvote");
 let numberOfVotes = document.getElementById("votes");
 let hide = document.getElementById("hide");
